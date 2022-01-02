47285cb0a903873cf2767aa0e5c03b5971871898
Manczak Adrian <manczakadrian@gmail.com>
2022-01-02 16:05:17 +0100

    Initial commit

---
+++ src/main/java/pl/manczak/amigosrestmvc/AmigosRestMvcApplication.java
@@ -0,0 +1,17 @@
+package pl.manczak.amigosrestmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class AmigosRestMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmigosRestMvcApplication.class, args);
+
+	}
+
+
+}
